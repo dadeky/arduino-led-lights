e9054b8f7e2d4a856359f4eab0ed13a55b708656
dvorski <dadeky@gmail.com>
2018-01-08 08:42:19 +0100

    Kicad and stuff

---
+++ LedLights/Bluetooth.cpp
@@ -0,0 +1,13 @@
+/*
+ * Bluetooth.cpp
+ *
+ *  Created on: Jan 5, 2018
+ *      Author: dvorski
+ */
+
+#include "Bluetooth.h"
+
+Bluetooth::Bluetooth() {
+	// TODO Auto-generated constructor stub
+}
+
---
+++ LedLights/Bluetooth.h
@@ -0,0 +1,18 @@
+/*
+ * Bluetooth.h
+ *
+ *  Created on: Jan 5, 2018
+ *      Author: dvorski
+ */
+
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+#include "WString.h"
+
+class Bluetooth {
+public:
+	Bluetooth();
+	void SetParameters(String name);
+};
+
+#endif /* BLUETOOTH_H_ */
